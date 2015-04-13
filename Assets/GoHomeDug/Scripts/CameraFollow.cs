b9de38f867e780cc9ace480a9d23bebf3a76059e
awong <awong@Andrew.local>
2015-04-13 14:12:34 -0700

    Made changes to test layout, refactoring player code

--- Assets/GoHomeDug/Scripts/CameraFollow.cs
+++ Assets/GoHomeDug/Scripts/CameraFollow.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
   protected GameObject player;
+  protected float leftBound = 0f; // Assume map always ends at x = 0
+  protected float rightBound;
 
   // Use this for initialization
   protected void Start () {
     player = GameObject.Find("basketball");
+    rightBound = player.transform.position.x;
   }
   
+  // Update camera to follow player
   protected void FixedUpdate () {
     Vector3 myPos = transform.position;
-    myPos.x = player.transform.position.x;
+    myPos.x = Mathf.Min(player.transform.position.x, rightBound);
+    myPos.x = Mathf.Max(myPos.x, leftBound);
     myPos.y = player.transform.position.y;
     transform.position = myPos;
   }
--- Assets/GoHomeDug/Scripts/Player.cs
+++ Assets/GoHomeDug/Scripts/Player.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
   bool isJump = false;
-  float deltaVx = 0.5f;
-  float maxSpeed = 4f;
-  float jumpVelocity = 4f;
+  float deltaVx = 0.3f;
+  float maxSpeed = 3f;
+  float jumpVelocity = 5f;
   float scaleSize;
 
   // Use this for initialization
@@ -27,10 +27,17 @@ public class Player : MonoBehaviour {
   // Update velocity on each timestep
   void UpdateVelocities() {
     Vector3 vel = rigidbody.velocity;
-    Debug.Log(vel);
     bool right = Input.GetKey(KeyCode.RightArrow);
     bool left = Input.GetKey(KeyCode.LeftArrow);
     bool up = Input.GetKey(KeyCode.UpArrow);
+    HandleKeyMovements(left, right, up, ref vel);
+    DampenXVelocity(left, right, ref vel);
+    TestCharacterIsJump(vel);
+    rigidbody.velocity = vel;
+  }
+
+  // Update velocity based on left, right, up press
+  void HandleKeyMovements(bool left, bool right, bool up, ref Vector3 vel) {
     if (right) {
       vel.x = Mathf.Min(vel.x + deltaVx, maxSpeed); 
       ChangeScaleX(1.0f);
@@ -43,6 +50,10 @@ public class Player : MonoBehaviour {
       vel.y = jumpVelocity;
       isJump = true;
     }
+  }
+
+  // Dampen x velocity if left and keys are not pressed and character is on floor.
+  void DampenXVelocity(bool left, bool right, ref Vector3 vel) {
     if (!right && !left && !isJump) { // dampen x velocity
       if (vel.x < 0) {
         vel.x = Mathf.Min(vel.x + deltaVx, 0.0f);
@@ -50,10 +61,13 @@ public class Player : MonoBehaviour {
         vel.x = Mathf.Max(vel.x - deltaVx, 0.0f);
       }
     }
-    if (vel.y != 0.0f) {
+  }
+
+  // Handle case where character falls off platform without jumping
+  void TestCharacterIsJump(Vector3 vel) {
+    if (vel.y != 0.0f && !isJump) {
       isJump = true;
     }
-    rigidbody.velocity = vel;
   }
 
   // Change direction of character.
@@ -66,7 +80,7 @@ public class Player : MonoBehaviour {
   // Detect whether character is on ground (and can jump)
   void OnCollisionStay (Collision col) {
     foreach (ContactPoint c in col.contacts) {
-      if (c.normal.y > 0.8f) {
+      if (c.normal.y > 0.9f) {
         isJump = false;
       }
     }
