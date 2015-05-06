0c10dc367414224bb46645137f4dd2e2e9ed645b
Alvin <alwong8@berkeley.edu>
2015-05-06 13:09:06 -0700

    final

--- Assets/Scripts/TextAppear.cs
+++ Assets/Scripts/TextAppear.cs
@@ -6,11 +6,13 @@ public class TextAppear : MonoBehaviour {
 	public float timeDelay;
 	public float timePeriod;
 
+	public bool last = false;
+
 	GUIStyle style;
 	bool used;
 	Text dialogue;
 	GameState state;
-	bool fade; //true is in, false is out
+	bool fade = true; //true is in, false is out
 	float visibleTimer = 0.0f;
 	float totalTime = 0.0f;
 
@@ -20,7 +22,6 @@ public class TextAppear : MonoBehaviour {
 		dialogue = GetComponent<Text> ();
 		style = new GUIStyle ();
 		style.wordWrap = true;
-		fade = true;
 	}
 
 	// Use this for initialization
@@ -30,11 +31,12 @@ public class TextAppear : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		totalTime += Time.deltaTime;
+		if (fade) {
+			totalTime += Time.deltaTime;
+		}
 		if (!used && visibleTimer == 0.0f && totalTime > timeDelay) {
 			Color c = dialogue.color;
 			if (fade) { //while text fades in 
-				state.SetState (GameState.State.NOCONTROLS);
 				c.a = Mathf.Min(1.0f, c.a + 0.6f * Time.deltaTime);
 				dialogue.color = c;
 				if (dialogue.color.a == 1.0f) {
@@ -42,11 +44,14 @@ public class TextAppear : MonoBehaviour {
 					visibleTimer = timePeriod;
 				}
 			} else {
-				c.a = Mathf.Max(0.0f, c.a - 2.0f * Time.deltaTime);
+				c.a = Mathf.Max(0.0f, c.a - 1.0f * Time.deltaTime);
 				dialogue.color = c;
 				if (dialogue.color.a == 0.0f) {
 					fade = true;
 					used = true;
+					if (last) {
+						state.SetState(GameState.State.END);
+					}
 				}
 			}
 		}
